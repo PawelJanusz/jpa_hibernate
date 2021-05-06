9b7c0129db67060c9d7c8b4ffd4c6ddcac27a4fc
Pawel Janusz <marek80k@wp.pl>
2021-05-06 22:01:53 +0200

    jpa model

---
+++ src/main/java/entity/jpa/Person.java
@@ -0,0 +1,66 @@
+package entity.jpa;
+
+import javax.persistence.Embedded;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private String surname;
+
+    @Embedded
+    private Address address;
+
+    private Address billingAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address=" + address +
+                ", billingAddress=" + billingAddress +
+                '}';
+    }
+}

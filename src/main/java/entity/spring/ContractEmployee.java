c0e8ba2255da4d5fba8037457920096cfdc672b0
Pawel Janusz <marek80k@wp.pl>
2021-05-07 18:26:39 +0200

    spring model

---
+++ src/main/java/entity/spring/ContractEmployee.java
@@ -0,0 +1,14 @@
+package entity.spring;
+
+public class ContractEmployee {
+
+    private Float payPerHour;
+
+    public Float getPayPerHour() {
+        return payPerHour;
+    }
+
+    public void setPayPerHour(Float payPerHour) {
+        this.payPerHour = payPerHour;
+    }
+}
---
+++ src/main/java/entity/spring/Pencil.java
@@ -0,0 +1,23 @@
+package entity.spring;
+
+public class Pencil {
+
+    private String color;
+    private boolean rubber;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isRubber() {
+        return rubber;
+    }
+
+    public void setRubber(boolean rubber) {
+        this.rubber = rubber;
+    }
+}
